16612495b888ae558abc1ae276cb0c3916c99638
kazvee <kazveedev@gmail.com>
2023-12-22 21:20:26 -0700

    Add links to Home page for the new routes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
-export default function Home() {
+import Link from 'next/link';
+
+const Home = () => {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <p>
+        🍛
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        💌 <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
+        🌎
+        <Link href="/community">Community</Link>
+      </p>
     </main>
   );
-}
+};
+
+export default Home;
