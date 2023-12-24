0ce64368b457c1a62f1da4a5becf9dc1285e4900
kazvee <kazveedev@gmail.com>
2023-12-24 12:43:48 -0700

    Add content and styling to Community page

--- app/community/page.js
+++ app/community/page.js
@@ -1,5 +1,42 @@
+import Image from 'next/image';
+
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+import classes from './page.module.css';
+
 const CommunityPage = () => {
-  return <h1>🌎 Community</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>✨ Community Perks ✨</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="A delicious meal" />
+            <p>📚 Share & discover recipes</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <p>👋 Find new friends & like-minded people</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p>🤩 Participate in exclusive events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default CommunityPage;
