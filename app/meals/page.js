dfe72c95a07b136b6744f8eb976d7bb74b22db11
kazvee <kazveedev@gmail.com>
2023-12-24 13:50:49 -0700

    Add content and styling to Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+
 const MealsPage = () => {
-  return <h1>🍛 Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you!</span> 🤩
+        </h1>
+        <p>Choose your favourite recipe and cook it yourself! 😃</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">✨ Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default MealsPage;
