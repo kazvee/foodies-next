748060fc460f052f573ff281b0221f89c26c328b
kazvee <kazveedev@gmail.com>
2023-12-25 16:06:07 -0700

    Add content and styling to Meal Details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,29 @@
+// import Image from 'next/image';
+import classes from './page.module.css';
+
 const MealDetailsPage = () => {
-  return <h1>🍲 Meal Details</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          {/* <Image fill /> */}
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: 'Instructions' }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
