e3fde7bee772afd380713cd92f2acbf689ac2eed
kazvee <kazveedev@gmail.com>
2023-12-25 15:46:24 -0700

    Add handling for Not Found states

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>⚡ Not Found ⚡</h1>
+      <p>Apologies, we cannot find the requested page or resource. 😕</p>
+    </main>
+  );
+};
+
+export default NotFound;
