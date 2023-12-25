03a36f60537729b64b08b73a0d433c9fce160adf
kazvee <kazveedev@gmail.com>
2023-12-25 15:35:05 -0700

    Add error handling

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+'use client';
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>⚡ Error ⚡</h1>
+      <p>Waffley sorry about this... 🧇</p>
+      <p>Failed to fetch meal data from the database. ☹️ Please try again later...</p>
+    </main>
+  );
+};
+
+export default Error;
