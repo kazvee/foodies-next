25099472b61f21662d4d85e8e9b85aadbf688391
kazvee <kazveedev@gmail.com>
2023-12-24 21:04:36 -0700

    Display data fetched from DB on the Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import getMeals from '@/lib/meals';
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +19,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
