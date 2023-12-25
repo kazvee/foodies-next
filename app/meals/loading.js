7593a3659021045bdce56045c2c51b85820385b9
kazvee <kazveedev@gmail.com>
2023-12-24 21:10:53 -0700

    Add loading page with styling

---
+++ app/meals/loading.js
@@ -0,0 +1,7 @@
+import classes from './loading.module.css';
+
+const MealsLoadingPage = () => {
+  return <p className={classes.loading}>Fetching Meals...</p>;
+};
+
+export default MealsLoadingPage;
