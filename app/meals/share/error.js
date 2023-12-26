2f468cecd0ef81aa8f72c070799bdebd4ccc6ebc
kazvee <kazveedev@gmail.com>
2023-12-26 14:18:16 -0700

    Add error handling for Share Meal

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>⚡ Error ⚡</h1>
+      <p>Failed to create meal. ☹️ Please try again later...</p>
+    </main>
+  );
+};
+
+export default Error;
