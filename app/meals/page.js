cf9a5c7b74e189a110d20c57035286f6d92363ae
kazvee <kazveedev@gmail.com>
2023-12-26 15:51:22 -0700

    Add static metadata to the All Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 const Meals = async () => {
   const meals = await getMeals();
 
